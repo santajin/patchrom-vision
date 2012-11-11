3fd0972ad7c5f8dd885af8527777234980c96992
santajin <santajin@santajin.(none)>
2012-11-11 10:06:18 +0800

    MIUI 2.11.9

--- releasetools.py
+++ releasetools.py
@@ -8,7 +8,7 @@ def DeleteFormat(info):
             edify.script[i]="""format("ext4", "EMMC", "/dev/block/mmcblk0p25", "0");"""
         elif "mount" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
             edify.script[i]="""mount("ext4", "EMMC", "/dev/block/mmcblk0p25", "/system");
-delete_recursive("/system/lib", "/system/app", "/system/bin", "/system/customize", "/system/etc", "/system/fonts", "/system/framework", "/system/media", "/system/tts", "/system/usr", "/system/xbin","/system/build.prop", "0");
+delete_recursive("/system/lost+found", "/system/lib", "/system/app", "/system/bin", "/system/customize", "/system/etc", "/system/fonts", "/system/framework", "/system/media", "/system/tts", "/system/usr", "/system/xbin","/system/build.prop", "0");
 run_program("/sbin/busybox", "rm", "/system/lib/*", "-rf");"""
 
     return
@@ -33,9 +33,21 @@ def FullOTA_InstallEnd(info):
     DeleteFormat(info)
     AddAssertions(info)
 
+def AddIncrementalLibrary(info):
+    info.output_zip.writestr("lib/libshell_jni.so", info.target_zip.read("SYSTEM/lib/libshell_jni.so"))
+    info.output_zip.writestr("lib/libshellservice.so", info.target_zip.read("SYSTEM/lib/libshellservice.so"))
+    info.output_zip.writestr("lib/libshell.so", info.target_zip.read("SYSTEM/lib/libshell.so"))
+    info.output_zip.writestr("lib/libaudiofp.so", info.target_zip.read("SYSTEM/lib/libaudiofp.so"))
+    info.output_zip.writestr("lib/libjni_resource_drm.so", info.target_zip.read("SYSTEM/lib/libjni_resource_drm.so"))
+    info.output_zip.writestr("lib/libffmpeg_xm.so", info.target_zip.read("SYSTEM/lib/libffmpeg_xm.so"))
+    info.output_zip.writestr("lib/liblocSDK_2.5OEM.so", info.target_zip.read("SYSTEM/lib/liblocSDK_2.5OEM.so"))
+    info.output_zip.writestr("lib/libjni_latinime.so", info.target_zip.read("SYSTEM/lib/libjni_latinime.so"))
+    info.script.AppendExtra('package_extract_dir("lib","/system/lib");');
+
 def IncrementalOTA_InstallEnd(info):
     edify = info.script
     for i in xrange(len(edify.script)):
         if "mount" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
             edify.script[i]="""mount("ext4", "EMMC", "/dev/block/mmcblk0p25", "/system");"""
+    AddIncrementalLibrary(info)
     AddAssertions(info)
